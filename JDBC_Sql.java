d635b08747740a47689ffb1d9e31b4f187a48c20
Sanjithj <sanjithj591997@gmail.com>
2022-10-05 23:32:12 +0530

    Added: insert crud operation

--- JDBC_Sql.java
+++ JDBC_Sql.java
@@ -15,7 +15,11 @@ public class JDBC_Sql {
 	      create table Address_Book_Table(firstName varchar(12) ,
 	      lastName varchar(15) , address varchar(50),city varchar(10),
 	      state varchar(15), zip int(15) , phoneNumber int(15) , email varchar(50));
-
+	      
+	    //**************Ability to insert new contact************
+	      insert into Address_Book_Table values(Sanjith","j","Arsikere","Hassan","karnatka",546463,768768667,"sanjith@gmail.com");
+	      insert into Address_Book_Table values("Karan","Kanaka","Wangova","Miraz","Argentina",686333,867686864,"sanji@gmail.com");
+	      insert into Address_Book_Table values("Bheema","Rana","jalawar","capa","Mexico",786768,572552211,"rana@gmail.com");
 
 	}
 
