778908dd67e7a5bf564e99dd4c6ec7ad02ef51c3
Sanjithj <sanjithj591997@gmail.com>
2022-10-05 23:29:33 +0530

    Added: payroll table

--- JDBC_Sql.java
+++ JDBC_Sql.java
@@ -9,6 +9,13 @@ public class JDBC_Sql {
 		//**********Ability to create database***********
 		  create database Address_Book;
 	      use Address_Book;
+	    
+	    // *********ability to create table************
+	      
+	      create table Address_Book_Table(firstName varchar(12) ,
+	      lastName varchar(15) , address varchar(50),city varchar(10),
+	      state varchar(15), zip int(15) , phoneNumber int(15) , email varchar(50));
+
 
 	}
 
