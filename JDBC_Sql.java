ae458a03a2a86f8ee71632dc14b5e264f4d8d0cb
Sanjithj <sanjithj591997@gmail.com>
2022-10-05 23:27:19 +0530

    Added: database created

--- JDBC_Sql.java
+++ JDBC_Sql.java
@@ -5,6 +5,11 @@ public class JDBC_Sql {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Jdbc crud operation in Sql");
+		
+		//**********Ability to create database***********
+		  create database Address_Book;
+	      use Address_Book;
+
 	}
 
 }
